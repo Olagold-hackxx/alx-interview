b57fb00f93e1e2378661d235a11ebc91206beb78
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-05 21:17:50 -0400

    Lockboxed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,12 +7,10 @@ def canUnlockAll(boxes):
     unlocked_boxes = [0]
 
     for box in boxes:
-        if len(box) >= 1:
-            key = box[0]
-        else:
-            key = 0
-        for j in range(1, len(boxes)):
-            if key not in unlocked_boxes and key == j:
-                unlocked_boxes.append(key)
+        for key in box:
+            for j in range(1, len(boxes)):
+                if key not in unlocked_boxes and key == j:
+                    unlocked_boxes.append(key)
+            break
 
     return len(unlocked_boxes) == len(boxes)
