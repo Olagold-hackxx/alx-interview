d35b81d133e1bdc3bb26caebe49ef39ce9e6c85e
Doyinsola Aladeloye <doyinsolaaladeloye@gmail.com>
2023-11-24 04:36:53 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
 """ Read stdout"""
 import sys
-from collections import Counter
 
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+def print_stats(total_size, status_codes):
+    print(f"Total file size: {total_size}")
+    for code in sorted(status_codes):
+        print(f"{code}: {status_codes[code]}")
 
+def process_line(line, total_size, status_codes):
+    parts = line.split()
+    if len(parts) != 7:
+        return total_size, status_codes
 
-def print_stdin(st_codes):
-    """Print allowed status codes"""
-    counter = Counter(st_codes)
-    for status, count in counter.items():
-        if status in status_codes:
-            print(f"{status}: {count}")
+    ip, _, _, status_code, size = parts
+    if not status_code.isdigit():
+        return total_size, status_codes
 
+    status_code = int(status_code)
+    if status_code not in [200, 301, 400, 401, 403, 404, 405, 500]:
+        return total_size, status_codes
 
-count = 0
-file_size = 0
-status_codes = []
-try:
-    for input in sys.stdin:
-        count += 1
-        required_input = input.split(" ")
-        if count == 10:
-            print("File size: {}".format(file_size))
-            status_codes.sort()
-            print_stdin(status_codes)
-            status_codes = []
-            count = 0
-        else:
-            if len(required_input) < 9:
-                continue
-            else:
-                status_code = int(required_input[7])
-                status_codes.append(status_code)
-                file_size += int(required_input[8])
-except KeyboardInterrupt:
-    print("File size: {}".format(file_size))
-    status_codes.sort()
-    print_stdin(status_codes)
-    sys.exit(0)
+    total_size += int(size)
+    status_codes[status_code] = status_codes.get(status_code, 0) + 1
+    return total_size, status_codes
+
+def main():
+    total_size = 0
+    status_codes = {}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            total_size, status_codes = process_line(line.strip(), total_size, status_codes)
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+                print()
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
